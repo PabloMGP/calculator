72c94361b24f3e0e780389658445b20c1f3ac7db
Pablo <pgarciacardone@gmail.com>
2023-04-10 19:26:43 +0100

    Update calculator functionality from scratch
    Previous code was functional for simple operations but this code will
    be simpler and more functional.
    
    Uses an object-oriented approach with updated functions to handle
    complex calculations.
    
    Part of this alternative approach includes incorporating value property
    in HTML elements for easier data manipulation, at least for me!

--- app.js
+++ app.js
@@ -1,126 +1,69 @@
+window.addEventListener("DOMContentLoaded", () => {
+});
+
 // Operator functions
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    return a / b;
+  return a / b;
 }
 
-// Needed to parse and join the stored values in "a" and "b" to integers for operate() function
-// to be able to utilise and pass down to the appropriate operator functions
-function parser(a, b) {
-    a = parseInt(a.join(""));
-    b = parseInt(b.join(""));
-    return [a, b];
+let calculator = {
+  displayValue: "0",
+  firstOperand: null,
+  isSecondOperand: false,
+  operator: null
+};
+
+function updateDisplay() {
+  let display = document.querySelector(".display");
+  display.value = calculator.displayValue;
 }
 
-// Resets values back to default 
-function resetValues() {
-    a = [];
-    b = [];
-    operator = "";
+function inputDigit(digit) {
+  const { displayValue } = calculator;
+  calculator.displayValue = displayValue === "0" ? digit : displayValue + digit;
 }
 
-function resetAll() {
-    resetValues();
-    result = null; // Resets the result variable
-    display.innerHTML = "Enter a calculation!";
-}
+updateDisplay();
 
-function operate(a, b, operator, result) {
-    [a, b] = parser(a, b);
-    if (operator === "+") {
-        if (result !== null) {
-            result = add(result, b);
-        } else {
-            result = add(a, b);
-        }
-        display.innerHTML = result; 
-    } else if (operator === "-") {
-        if (result !== null) {
-            result = subtract(result, b);
-        } else {
-            result = subtract(a, b);
-        }
-        display.innerHTML = result; 
-    } else if (operator === "x") {
-        if (result !== null) {
-            result = multiply(result, b);
-        } else {
-            result = multiply(a, b);
-        }
-        display.innerHTML = result; 
-    } else if (operator === "/") {
-        if (result !== null) {
-            result = divide(result, b);
-        } else {
-            result = divide(a, b);
-        }
-        display.innerHTML = result; 
-    } else {
-        console.log("Error!");
-        return null;
+
+const buttons = document.querySelectorAll('.calculator-btn');
+buttons.forEach(element => {
+  element.addEventListener("click", (event) => {
+    // Access the clicked element
+    const { target } = event;
+
+    // Check if the clicked element is a button.
+    // If not, exit from the function
+    if (!target.matches("button")) {
+      return;
+    } if (target.classList.contains("operator")) {
+      inputDigit(target.value);
+      updateDisplay();
+      return;
+    } if (target.classList.contains("number")) {
+      inputDigit(target.value);
+      updateDisplay();
+      return;
+    } if (target.classList.contains("clear")) {
+      inputDigit(target.value);
+      updateDisplay();
+      return;
     }
-    return result;
-}
-
-let a = [];
-let b = [];
-let operator = "";
-let result = null;
-let cycle = 0; // Added a cycle variable to keep track of button presses
-
-let numberBtns = document.querySelectorAll(".number");
-let operatorBtns = document.querySelectorAll(".operator");
-let equalBtn = document.querySelector(".equal");
-let clearBtn = document.querySelector(".clear");
-let display = document.querySelector(".display");
-
-// Number buttons
-numberBtns.forEach(button => {
-    button.addEventListener("click", (e) => {
-        if (operator === "") { // Only push to "a" if operator hasn't been selected yet
-            a.push(e.target.innerHTML);
-            display.innerHTML = a.join("");
-        } else {
-            b.push(e.target.innerHTML);
-            display.innerHTML = b.join("");
-        }
-    });
+  })
 });
 
-// Operator buttons
-operatorBtns.forEach(button => {
-    button.addEventListener("click", (e) => {
-        operator = e.target.innerHTML;
-        display.innerHTML = operator;
-    })
-});
 
-// Equal button
-equalBtn.addEventListener("click", () => {
-    cycle += 1;
-    if (cycle === 1) { // If this is the first time pressing equals, save the first number to result
-        result = operate(a, b, operator, result);
-        resetValues();
-    } else if (cycle >= 2) { // If this is the second time or more pressing equals, perform the operation
-        result = operate(a, b, operator, result);
-        resetValues();
-    } else {
-        console.log("Error at equalBtn click");
-    }
-});
 
-// AC button
-clearBtn.addEventListener("click", () => {
-    resetAll();
-});
+
